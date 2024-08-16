7acbd1f5c4e4294f8fe0eb4a703bc22e917f2c57
Denis Krasniqi <34380779+denikrasniqi@users.noreply.github.com>
2024-08-16 14:51:57 +0200

    Update filterApi.py

--- filterApi.py
+++ filterApi.py
@@ -7,7 +7,7 @@ import psycopg2
 import time
 from flask import Flask, jsonify, request
 import logging
-from airpristina import run_airprishtina_ticket_script
+# from airpristina import run_airprishtina_ticket_script
 from flyska import run_flyska_ticket_script
 from kosfly import run_kosfly_ticket_script
 from prishtinaticket import run_prishtina_ticket_script
@@ -59,7 +59,6 @@ def run_scripts():
         # List of script functions to run
         scripts = [
             run_prishtina_ticket_script,
-            run_airprishtina_ticket_script,
             run_flyska_ticket_script,
             run_kosfly_ticket_script,
             run_flyrbp_ticket_script
