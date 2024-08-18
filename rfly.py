05db54592adb815ba95157cad8e0ce066420d0e1
Kushtrim Shabani <63112533+KushtrimShabani@users.noreply.github.com>
2024-08-18 02:24:48 +0200

    Update rfly.py

--- rfly.py
+++ rfly.py
@@ -46,7 +46,7 @@ def extract_flight_info(page_html, target_date):
     return flights
 
 def run_flyrbp_ticket_script():
-      airport_pairs = [
+    airport_pairs = [
         ('MLH', 'PRN'),
         ('PRN', 'DUS'),
         ('PRN', 'MUC'),
@@ -55,6 +55,8 @@ def run_flyrbp_ticket_script():
         ('STR', 'PRN'),
         ('PRN', 'MLH'),
         ('MUC', 'PRN'),
+        ('PRN', 'NUE'),
+        ('NUE', 'PRN'),
     ]
 
     for departure, arrival in airport_pairs:
