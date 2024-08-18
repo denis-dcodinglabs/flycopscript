afc0a9be75783f724022a18b9b5ff71c24a664c5
Denis Krasniqi <34380779+denikrasniqi@users.noreply.github.com>
2024-08-18 14:43:46 +0200

    Update prishtinaticket.py

--- prishtinaticket.py
+++ prishtinaticket.py
@@ -115,7 +115,7 @@ def run_prishtina_ticket_script():
     }
 
     for departure, arrival in airport_pairs:
-        for day in range(1, 8):
+        for day in range(0, 8):
             with sync_playwright() as p:
                 browser = p.chromium.launch(headless=True)
                 context = browser.new_context(
